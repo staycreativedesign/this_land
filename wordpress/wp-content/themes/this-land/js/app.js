4dc7febcbc5749829db47b5e325c91984f427612
Gustavo ParesY <gustavoanalytics@gmail.com>
2015-09-15 10:03:05 -0500

    added new design

--- wordpress/wp-content/themes/this-land/js/app.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($){
-	$('#menu').slicknav({
-		'label' : '',
-		'brand': 'This Land'
-	});
-    
-});
--- wordpress/wp-content/themes/this-land/js/bottom.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function($){
-    $('#menu').slicknav({
-    	'label' : '',
-    	'brand': '<img src="http://devthisland.wpengine.com/wp-content/uploads/2015/09/this-land-logo.png" width="91%">'
-    });
-});

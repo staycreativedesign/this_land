b55c787008dcfcc2f7f25aeff45be7cef476c8e1
Gustavo ParesY <gustavoanalytics@gmail.com>
2015-09-30 13:17:41 -0500

    commented out doubleclick

--- wordpress/wp-content/themes/this-land/header.php
+++ wordpress/wp-content/themes/this-land/header.php
@@ -10,6 +10,68 @@
 	        $('.slicknav_menu').prepend('<a href="/"><img class="logo" src="images/this-land-logo.png" alt="Website Logo" /></a>');
 	    });
 	</script>
+	<script type='text/javascript'>
+	  var googletag = googletag || {};
+	  googletag.cmd = googletag.cmd || [];
+	  (function() {
+	    var gads = document.createElement('script');
+	    gads.async = true;
+	    gads.type = 'text/javascript';
+	    var useSSL = 'https:' == document.location.protocol;
+	    gads.src = (useSSL ? 'https:' : 'http:') +
+	      '//www.googletagservices.com/tag/js/gpt.js';
+	    var node = document.getElementsByTagName('script')[0];
+	    node.parentNode.insertBefore(gads, node);
+	  })();
+	</script>
+
+	<script type='text/javascript'>
+	  googletag.cmd.push(function() {
+	    googletag.defineSlot('/4053978/DoWhat_OKC_120x240_1', [120, 240], 'div-gpt-ad-1443629402046-0').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/DoWhat_OKC_120x240_2', [120, 240], 'div-gpt-ad-1443629402046-1').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/DoWhat_OKC_120x240_3', [120, 240], 'div-gpt-ad-1443629402046-2').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/DoWhat_OKC_120x240_4', [120, 240], 'div-gpt-ad-1443629402046-3').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/DoWhat_OKC_336x280_L', [336, 280], 'div-gpt-ad-1443629402046-4').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/DoWhat_OKC_336x280_R', [336, 280], 'div-gpt-ad-1443629402046-5').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/DoWhat_OKC_336x550_L', [336, 550], 'div-gpt-ad-1443629402046-6').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/DoWhat_OKC_336x550_R', [336, 550], 'div-gpt-ad-1443629402046-7').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/DoWhat_OKC_895x80_Leaderboard', [895, 80], 'div-gpt-ad-1443629402046-8').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/DoWhat_Tulsa_120x240_1', [120, 240], 'div-gpt-ad-1443629402046-9').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/DoWhat_Tulsa_120x240_2', [120, 240], 'div-gpt-ad-1443629402046-10').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/DoWhat_Tulsa_120x240_3', [120, 240], 'div-gpt-ad-1443629402046-11').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/DoWhat_Tulsa_120x240_4', [120, 240], 'div-gpt-ad-1443629402046-12').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/DoWhat_Tulsa_336x280_L', [336, 280], 'div-gpt-ad-1443629402046-13').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/DoWhat_Tulsa_336x280_R', [336, 280], 'div-gpt-ad-1443629402046-14').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/DoWhat_Tulsa_336x550_L', [336, 550], 'div-gpt-ad-1443629402046-15').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/DoWhat_Tulsa_336x550_R', [336, 550], 'div-gpt-ad-1443629402046-16').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/DoWhat_Tulsa_895x80_Leaderboard', [895, 80], 'div-gpt-ad-1443629402046-17').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/This_Land_Press_Leaderboard_728x90', [728, 90], 'div-gpt-ad-1443629402046-18').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/ThisLand_Header_125x125_L', [125, 125], 'div-gpt-ad-1443629402046-19').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/ThisLand_Header_125x125_R', [125, 125], 'div-gpt-ad-1443629402046-20').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/ThisLand_Home_336x280_L', [336, 280], 'div-gpt-ad-1443629402046-21').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/ThisLand_Home_336x280_R', [336, 280], 'div-gpt-ad-1443629402046-22').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/ThisLand_Row_120x240_1', [120, 240], 'div-gpt-ad-1443629402046-23').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/ThisLand_Row_120x240_2', [120, 240], 'div-gpt-ad-1443629402046-24').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/ThisLand_Row_120x240_3', [120, 240], 'div-gpt-ad-1443629402046-25').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/ThisLand_Row_120x240_4', [120, 240], 'div-gpt-ad-1443629402046-26').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/ThisLand_Row_120x240_5', [120, 240], 'div-gpt-ad-1443629402046-27').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/ThisLand_Row_120x240_6', [120, 240], 'div-gpt-ad-1443629402046-28').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/ThisLand_Sidebar_120x240_L', [120, 240], 'div-gpt-ad-1443629402046-29').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/ThisLand_Sidebar_120x240_R', [120, 240], 'div-gpt-ad-1443629402046-30').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/ThisLand_Sidebar_336x280', [336, 280], 'div-gpt-ad-1443629402046-31').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/ThisLand_Sidebar_336x280_L1', [336, 280], 'div-gpt-ad-1443629402046-32').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/ThisLand_Sidebar_336x280_L2', [336, 280], 'div-gpt-ad-1443629402046-33').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/ThisLand_Sidebar_336x280_R1', [336, 280], 'div-gpt-ad-1443629402046-34').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/ThisLand_Sidebar_336x280_R2', [336, 280], 'div-gpt-ad-1443629402046-35').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/ThisLand_Sidebar_336x550', [336, 550], 'div-gpt-ad-1443629402046-36').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/ThisLand_Sidebar_336x550_L', [336, 550], 'div-gpt-ad-1443629402046-37').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/ThisLand_Sidebar_336x550_R', [336, 550], 'div-gpt-ad-1443629402046-38').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/ThisLand_Sidebar_Bottom_336x280', [336, 280], 'div-gpt-ad-1443629402046-39').addService(googletag.pubads());
+	    googletag.defineSlot('/4053978/ThisLand_Single_Leaderboard_Bottom', [728, 90], 'div-gpt-ad-1443629402046-40').addService(googletag.pubads());
+	    googletag.pubads().enableSingleRequest();
+	    googletag.enableServices();
+	  });
+	</script>
 </head>
 	<body>
 		<aside class="banner-wrapper-mobile">
